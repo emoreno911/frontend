f9390d1ff6aba53a47b1c70a26a0794f537a5ee1
Eduardo Moreno <e_moreno911@yahoo.com>
2013-11-04 22:01:06 -0430

    Class 3

---
+++ class3/javascript.js
@@ -0,0 +1,32 @@
+var a = 8, b = 4, arr = [1,2,3,4,5,6,7,8,9];
+
+var carro = {
+	color: '#473',
+	modelo: 'fiesta',
+	fabricante: 'ford',
+	vel_max: 100,
+	arranque : function() {
+		console.log('Ha arrancado...');
+	}
+};
+
+if(a > b){
+	console.log('A es mayor que B');
+	//console.log(arr);
+}else{
+	console.log('B es mayor que A');
+}
+
+/*for (var i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
+};*/
+
+// for(var i in arr){
+// 	var item = arr[i];
+// 	console.log(item);
+// }
+/*var i = 1;
+do{
+	console.log('hola')
+	i++;
+}while(i < 5)*/
